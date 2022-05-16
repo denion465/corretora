853c9b9fdf8d74febc380268a8c14e7a024804f4
Daniel Vidal <danielsv465@hotmail.com>
2022-05-15 23:36:15 -0300

    Tratamento de erros erros

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,32 @@
 import 'dotenv/config';
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 
 import { routes } from './routes';
+import AppError from './errors/AppError';
+
 import '../src/typeorm';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message
+    });
+  }
+
+  console.error(err);
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error'
+  });
+});
 
 app.listen(
   process.env.API_PORT || 3333,
